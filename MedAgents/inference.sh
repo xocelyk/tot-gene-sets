70ce40bb942e60b280bd266d6730cd0c468ca764
xocelyk <kylecox2000@gmail.com>
2024-02-12 12:25:26 -0600

    Importing my old work into new branch

--- MedAgents/inference.sh
+++
@@ -1,8 +0,0 @@
-python -u run.py \
---model_name chatgpt \
---dataset_name MedQA \
---dataset_dir ./datasets/MedQA/ \
---start_pos 0 \
---end_pos -1 \
---output_files_folder ./outputs/MedQA/ \
---max_attempt_vote 3
